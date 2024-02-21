312e6ad8d55d6b0824a8468f923ae07ea008b62c
nathlessa <nathlessasa@gmail.com>
2024-02-21 00:00:36 -0300

    changes photos and photos alt

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function toggleMode() {
   const html = document.documentElement
+  html.classList.toggle("light")
 
-  //if (html.classList.contains('light')) {
-  //  html.classList.remove('light')
-  //}
-  //else{
-  //html.classList.add('light')
-  //}
+  const img = document.querySelector("#profile img")
 
-  html.classList.toggle('light')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.png")
+  }
+
+   if (html.classList.contains("light")) {
+     img.setAttribute(
+       "alt",
+       "Foto Maykbrito sorrindo, usando óculos escuro e jaqueta de couro preta, sem barba e fundo em gradiente azul, rosa e roxo"
+     )
+   } else {
+     img.setAttribute(
+       "alt",
+       "Foto Maykbrito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+     )
+   }
 }
